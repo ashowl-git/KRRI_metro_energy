b75e9f75c4465f831c0a724803b1897620d07152
Ahn seung ho <ashaahn@gmail.com>
2023-08-07 18:43:42 +0900

    e

--- KRRI_METRO_ENERGY_PUB.py
+++ KRRI_METRO_ENERGY_PUB.py
@@ -55,21 +55,25 @@ st.markdown(hide_menu_style, unsafe_allow_html=True) # hide the hamburger menu?
 tab0, tab1, tab2 = st.tabs(['프로그램 사용자 메뉴얼','패시브-액티브 기술을 적용한 2차 에너지 성능 분석', 'ZEB 취득을 위한 신재생 에너지 제안'])
 
 #  필요한 데이터 불러오기
-
-@st.cache_data
-
-def get_sun_data(path):
-    data = pd.read_excel(path)
+def get_sun_data(path, sheet):
+    data = pd.read_excel(path, sheet_name=sheet)
     return data
 
-DF1 = get_sun_data('data/DB.xlsx', sheet_name='01_sun')
+
+DF1 = get_sun_data('data/DB.xlsx', '01_sun')
+DF2 = get_sun_data('data/DB.xlsx', '02_sun2')
+DF3 = get_sun_data('data/DB.xlsx', '03_clearsky')
+DF5 = get_sun_data('data/DB.xlsx', '04_renewable')
+DF6 = get_sun_data('data/DB.xlsx', '05_zero')
+DF7 = get_sun_data('data/DB.xlsx', '06_price')
+
 
 # DF1 = pd.read_excel('data/DB.xlsx', sheet_name='01_sun')  #일사량
-DF2 = pd.read_excel('data/DB.xlsx', sheet_name='02_sun2') #경사일사량
-DF3 = pd.read_excel('data/DB.xlsx', sheet_name='03_clearsky') # 맑은날
-DF5 = pd.read_excel('data/DB.xlsx', sheet_name='04_renewable') # 신재생
-DF6 = pd.read_excel('data/DB.xlsx', sheet_name='05_zero') # 신재생
-DF7 = pd.read_excel('data/DB.xlsx', sheet_name='06_price') # 가격
+# DF2 = pd.read_excel('data/DB.xlsx', sheet_name='02_sun2') #경사일사량
+# DF3 = pd.read_excel('data/DB.xlsx', sheet_name='03_clearsky') # 맑은날
+# DF5 = pd.read_excel('data/DB.xlsx', sheet_name='04_renewable') # 신재생
+# DF6 = pd.read_excel('data/DB.xlsx', sheet_name='05_zero') # 신재생
+# DF7 = pd.read_excel('data/DB.xlsx', sheet_name='06_price') # 가격
 
 with tab0 : 
     empty1, con1, empty2 = st.columns([0.1, 1.0, 0.1])
@@ -120,7 +124,13 @@ with tab0 :
 with tab1 : 
 
     # 학습파일 불러오기
-    df_raw = pd.read_excel('data/metro_sim_month.xlsx')
+    @st.cache_data
+    def get_sun_data(path):
+        data = pd.read_excel(path)
+        return data
+
+
+    # df_raw = pd.read_excel('data/metro_sim_month.xlsx')
 
     box_학습데이터_업로드 = st.checkbox('학습 데이터 업로드(필요시 체크)')
     if box_학습데이터_업로드 : 
